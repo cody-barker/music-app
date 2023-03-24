8a2f58757ed3c95529afaf29f9645bbc5a834559
Cody <codybarker.or@gmail.com>
2023-03-24 15:28:16 -0700

    creating db

---
+++ src/Player/audio.js
@@ -0,0 +1,14 @@
+exports.songsData = [
+    {
+        "title": "Breaking Through",
+        "url": "https://soundcloud.com/caregiver/breaking-through"
+    },
+    {
+        "title": "Bombay",
+        "url": "https://soundcloud.com/caregiver/bombay"
+    },
+    {
+        "title": "Mangoes",
+        "url": "https://soundcloud.com/caregiver/mangoes"
+    }
+]
